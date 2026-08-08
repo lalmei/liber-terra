6032e8a51da497160a5c94ebc6cb53c3b8cfdaff
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2026-08-07 20:03:03 -0500

    🔖 release: 0.3.0.

--- mod/src/LiberTerraModMetadata.cs
+++ mod/src/LiberTerraModMetadata.cs
@@ -3,6 +3,6 @@ namespace LiberTerra;
 public static class LiberTerraModMetadata
 {
     public const string ModId = "liberterra";
-    public const string Version = "0.2.51";
+    public const string Version = "0.3.0";
     public const string StartupLogMessage = "Liber Terra starting: pre-1300 lore library.";
 }
