f85a8f6736ed3d15ce6fdc94df6ca60954ed3267
Leandro G. Almeida <leandro.g.almeida@gmail.com>
2026-08-25 09:07:58 -0700

    🔖 release: prepare the READ A BOOK v0.6.0 release.

--- mod/src/LiberTerraModMetadata.cs
+++ mod/src/LiberTerraModMetadata.cs
@@ -3,6 +3,6 @@ namespace LiberTerra;
 public static class LiberTerraModMetadata
 {
     public const string ModId = "liberterra";
-    public const string Version = "0.5.0";
+    public const string Version = "0.6.0";
     public const string StartupLogMessage = "Liber Terra starting: pre-1300 lore library.";
 }
